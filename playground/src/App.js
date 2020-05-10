8bcdaaa5e2877c8d68e08ec24c35d31b291005d2
Saransh <saransh98@gmail.com>
2020-05-10 17:03:52 +0530

    done

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,11 +1,13 @@
-import React ,{Component} from 'react';
+import React,{Component} from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
+
+    const arr=["number1","number2","number3"]
     return (
-      <div>
-        hello
+      <div className="test1">
+        { arr.map(x=> <div key={x}>options are {x}</div> ) }
       </div>
     );
   }
---
+++ readme.txt
@@ -0,0 +1,3 @@
+1.run command in plaground folder 'npm instll'
+2.run command 'npm start'
+3.open url 'localhost:3000/'
